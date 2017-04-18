d8206fb0a17e6cdd9417e584ac0458accc7994df
jvhaoxianshen <736921448@qq.com>
2017-04-18 17:20:40 +0800

    添加图片

---
+++ js/index.js
@@ -0,0 +1,36 @@
+﻿/**
+ * Created by Administrator on 2017/3/29.
+ */
+window.onload = function() {
+
+    /*
+    * 图片框左右滑动事件
+    * */
+    var bannerSlider = document.getElementById('bannerSlider');
+    var leftBtn = document.getElementById('leftBtn');
+    var rightBtn = document.getElementById('rightBtn');
+
+    //滑动图片数量
+    var licount = bannerSlider.getElementsByTagName('li').length;
+    //给左按钮添加向左滑动事件
+    bannerSlider.style.left = '0px';
+    leftBtn.onclick = function() {
+
+        var huaDongJvLi = parseInt(bannerSlider.style.left) - 288;
+        if(huaDongJvLi >= -2016) {
+             $('.banner_slider ul').css('transition' , '0.8s' );
+            huaDong(huaDongJvLi);
+            
+        } else {
+            $('.banner_slider ul').css('transition' , '0s' );
+            huaDong(-288);
+        }
+        
+    }
+
+    function huaDong(huaDongJvLi){
+
+        bannerSlider.style.left = huaDongJvLi + 'px';
+
+    }
+}
